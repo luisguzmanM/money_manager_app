0a7b31e5001ffb8681da5ab0a4c5412a93ffe5e6
Luis Guzman <guzmanluis.lg@gmail.com>
2021-02-27 00:47:18 -0500

    Trying to repair bug when I want to update data in Localstorage

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ const getTransactionId = () => {
 const createRowItem = (formObj) => {
 
   const newRow = document.querySelector("#table").insertRow(-1);
+  newRow.setAttribute('data-transaction-id', formObj['transactionId']);
   let newCell = newRow.insertCell(0);
   newCell.textContent = formObj['typeSelection'];
   newCell = newRow.insertCell(1);
@@ -54,17 +55,37 @@ const createRowItem = (formObj) => {
   btnDeleteItem.appendChild(btnDeleteText);
   cellForBtn.appendChild(btnDeleteItem);
 
+  btnDeleteItem.addEventListener('click', function(){
+    let transactionRow = this.parentNode.parentNode;
+    let transactionId = transactionRow.getAttribute('data-transaction-id');
+    transactionRow.remove();
+    deleteItemLocalStorage(transactionId);
+  });
+
 }
 
 const saveData = (formObj) => {
+
   let savedData = JSON.parse(localStorage.getItem('transactions')) || [];
   savedData.push(formObj);
   localStorage.setItem('transactions', JSON.stringify(savedData));
+
+}
+
+const deleteItemLocalStorage = (transactionId) => {
+
+  let savedData = JSON.parse(localStorage.getItem('transactions'));
+  let indexDataToDelete = savedData.filter(x => x.transactionId === transactionId);
+  indexDataToDelete.splice(indexDataToDelete, 1);
+  localStorage.setItem('transactions', JSON.stringify(savedData ));
+
 }
 
 document.addEventListener('DOMContentLoaded', function(){
+
   let myArray = JSON.parse(localStorage.getItem('transactions')) || [];
   myArray.forEach(x => {
     createRowItem(x);
   });
+
 });
