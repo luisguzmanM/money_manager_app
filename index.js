ffabf64c8a4716d4b7c30d222e9bc213d36f88a4
Luis Guzman <guzmanluis.lg@gmail.com>
2021-02-26 23:02:17 -0500

    Add function to: Add element to row and save data in localStorage,

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ form.addEventListener('submit', function(e){
   e.preventDefault();
   const formData = new FormData(form);
   const formObj = formToObject(formData);
-  console.log(formObj);
+  createRowItem(formObj);
+  saveData(formObj);
+
 });
 
 const formToObject = (formData) => {
@@ -26,3 +28,43 @@ const formToObject = (formData) => {
 
 }
 
+const getTransactionId = () => {
+
+  let lastTransactionId = localStorage.getItem('lastTransactionId') || '-1';
+  let newTransactionId = JSON.parse(lastTransactionId) + 1;
+  localStorage.setItem('lastTransactionId', JSON.stringify(newTransactionId));
+  return newTransactionId;
+
+}
+
+const createRowItem = (formObj) => {
+
+  const newRow = document.querySelector("#table").insertRow(-1);
+  let newCell = newRow.insertCell(0);
+  newCell.textContent = formObj['typeSelection'];
+  newCell = newRow.insertCell(1);
+  newCell.textContent = formObj['description'];
+  newCell = newRow.insertCell(2);
+  newCell.textContent = formObj['amount'];
+  newCell = newRow.insertCell(3);
+  newCell.textContent = formObj['category'];
+  let cellForBtn = newRow.insertCell(4);
+  const btnDeleteItem = document.createElement('button');
+  const btnDeleteText = document.createTextNode('Delete');
+  btnDeleteItem.appendChild(btnDeleteText);
+  cellForBtn.appendChild(btnDeleteItem);
+
+}
+
+const saveData = (formObj) => {
+  let savedData = JSON.parse(localStorage.getItem('transactions')) || [];
+  savedData.push(formObj);
+  localStorage.setItem('transactions', JSON.stringify(savedData));
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  let myArray = JSON.parse(localStorage.getItem('transactions')) || [];
+  myArray.forEach(x => {
+    createRowItem(x);
+  });
+});
