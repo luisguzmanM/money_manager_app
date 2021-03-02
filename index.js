572e9e1ef958d8a8c5774c42b78f404f9190acac
Luis Guzman <guzmanluis.lg@gmail.com>
2021-03-01 19:47:54 -0500

    add simple validation with html

--- index.js
+++ index.js
@@ -1,47 +1,51 @@
 const form = document.querySelector("#form");
+const inputTypeSelection = document.getElementsByTagName('typeSelection');
+const inputDescription = document.getElementById('description');
+const inputAmount = document.getElementById('amount');
+const inputCategory = document.getElementById('category');
 
-form.addEventListener('submit', function(e){
+const addTask = (e) => {
 
   e.preventDefault();
   const formData = new FormData(form);
   const formObj = formToObject(formData);
   createRowItem(formObj);
   saveData(formObj);
-  form.reset()
+  form.reset();
 
-});
-   
+}
+
 const formToObject = (formData) => {
-
+  
   let typeSelection = formData.get('typeSelection');
   let description = formData.get('description');
   let amount = formData.get('amount');
   let category = formData.get('category');
   let id = getTransactionId();
-
+  
   return {
-
+    
     'typeSelection':typeSelection,
     'description':description,
     'amount':amount,
     'category':category,
     'id':id
-
+    
   }
-
+  
 }
 
 const getTransactionId = () => {
-
+  
   let lastTransactionId = localStorage.getItem('lastTransactionId') || '-1';
   let newTransactionId = JSON.parse(lastTransactionId) + 1;
   localStorage.setItem('lastTransactionId', JSON.stringify(newTransactionId));
   return newTransactionId;
-
+  
 }
 
 const createRowItem = (formObj) => {
-
+  
   const newRow = document.querySelector("#table").insertRow(-1);
   newRow.setAttribute('id', formObj['id']);
   let newCell = newRow.insertCell(0);
@@ -49,7 +53,7 @@ const createRowItem = (formObj) => {
   newCell = newRow.insertCell(1);
   newCell.textContent = formObj['description'];
   newCell = newRow.insertCell(2);
-  newCell.textContent = formObj['amount'];
+  newCell.textContent = '$' + formObj['amount'];
   newCell = newRow.insertCell(3);
   newCell.textContent = formObj['category'];
   let cellForBtn = newRow.insertCell(4);
@@ -57,20 +61,20 @@ const createRowItem = (formObj) => {
   const btnDeleteText = document.createTextNode('Delete');
   btnDeleteItem.appendChild(btnDeleteText);
   cellForBtn.appendChild(btnDeleteItem);
-
+  
   btnDeleteItem.addEventListener('click', function(){
-
+    
     let transactionRow = this.parentNode.parentNode;
     let id = transactionRow.getAttribute('id');
     deleteItemLocalStorage(id);
     transactionRow.remove();
-
+    
   });
-
+  
 }
 
 const saveData = (formObj) => {
-
+  
   let savedData = JSON.parse(localStorage.getItem('transactions')) || [];
   savedData.push(formObj);  
   localStorage.setItem('transactions', JSON.stringify(savedData));
@@ -78,19 +82,21 @@ const saveData = (formObj) => {
 }
 
 const deleteItemLocalStorage = (id) => {
-
+  
   let savedData = JSON.parse(localStorage.getItem('transactions')) || [];
   let getIndexElem = savedData.findIndex(x => x.id == id);
   savedData.splice(getIndexElem, 1);
   localStorage.setItem('transactions', JSON.stringify(savedData));
-
+  
 }
 
 document.addEventListener('DOMContentLoaded', function(){
-
+  
   let myArray = JSON.parse(localStorage.getItem('transactions')) || [];
   myArray.forEach(x => {
     createRowItem(x);
   });
-
+  
 });
+
+form.addEventListener('submit', addTask);
